b3b892f6c265e835f46286fb6f869813be7fed22
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-03-31 21:19:20 +0900

    fix

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -15,8 +15,11 @@ type UserData = {
   displayName: string;
   createdAt: Timestamp;
 };
-class UserDoc extends FireDocument<UserData> {
+class UserDoc extends FireDocument<UserData> implements UserData {
+  displayName: string;
+  createdAt: Timestamp;
   postsCollection = new PostsCollection(this.ref.collection("posts"));
+
   static create(data: Partial<UserData>): UserData {
     return {
       displayName: "MyString",
@@ -101,88 +104,206 @@ describe("unfireorm", () => {
     await clearFirestore();
   });
 
-  it("insert", async () => {
-    const data = UserDoc.create({ displayName: "user-1" });
-    const { id } = await usersCollection.insert(data);
+  describe("Collection", () => {
+    it("insert", async () => {
+      const data = UserDoc.create({ displayName: "user-1" });
+      const { id } = await usersCollection.insert(data);
 
-    const gotData = await usersRef
-      .doc(id)
-      .get()
-      .then((snap) => snap.data());
+      const gotData = await usersRef
+        .doc(id)
+        .get()
+        .then((snap) => snap.data());
 
-    expect(gotData).toStrictEqual(data);
-  });
-
-  it("insert (with id)", async () => {
-    const data = UserDoc.create({ displayName: "user-1" });
-    await usersCollection.insert({ id: "1", ...data });
-
-    const gotData = await usersRef
-      .doc("1")
-      .get()
-      .then((snap) => snap.data());
-
-    expect(gotData).toStrictEqual(data);
-  });
-
-  it("findOneById", async () => {
-    const data = UserDoc.create({ displayName: "user-1" });
-    const { id } = await usersRef.add(data);
-
-    const user = await usersCollection.findOneById(id);
-
-    expect(user.toData()).toStrictEqual(data);
-  });
-
-  it("findManyByQuery", async () => {
-    const data1 = UserDoc.create({ displayName: "user-1" });
-    const data2 = UserDoc.create({ displayName: "user-2" });
-
-    await usersRef.add(data1);
-    await usersRef.add(data2);
-
-    const [user1] = await usersCollection.findManyByQuery((ref) => ref.where("displayName", "==", "user-1"));
-
-    expect(user1.toData()).toStrictEqual(data1);
-  });
-
-  it("paginate", async () => {
-    const dataList = Array.from({ length: 10 }).map((_, idx) => {
-      let date = new Date("1999-12-31");
-      date = new Date(date.setDate(date.getDate() + idx));
-
-      const data = UserDoc.create({
-        displayName: `user-${idx}`,
-        createdAt: Timestamp.fromDate(date),
-      });
-      return data;
+      expect(gotData).toStrictEqual(data);
     });
 
-    await Promise.all(dataList.map((data) => usersRef.add(data)));
+    it("insert (with id)", async () => {
+      const data = UserDoc.create({ displayName: "user-1" });
+      await usersCollection.insert({ id: "1", ...data });
 
-    const all = await usersCollection.findAll({});
-    expect(all.edges.map((edge) => edge.node.toData())).toStrictEqual(dataList);
+      const gotData = await usersRef
+        .doc("1")
+        .get()
+        .then((snap) => snap.data());
 
-    const PAGE_SIZE = 2;
+      expect(gotData).toStrictEqual(data);
+    });
 
-    const firstPage = await usersCollection.findAll({ first: PAGE_SIZE });
-    const firstPageData = firstPage.edges.map((edge) => edge.node.toData());
-    expect(firstPageData).toStrictEqual(dataList.slice(0, 2));
+    it("findOneById", async () => {
+      const data = UserDoc.create({ displayName: "user-1" });
+      const { id } = await usersRef.add(data);
 
-    const secondPage = await usersCollection.findAll({ first: PAGE_SIZE, after: firstPage.pageInfo.endCursor });
-    const secondPageData = secondPage.edges.map((edge) => edge.node.toData());
-    expect(secondPageData).toStrictEqual(dataList.slice(2, 4));
+      const user = await usersCollection.findOneById(id);
 
-    const thirdPage = await usersCollection.findAll({ first: PAGE_SIZE, after: secondPage.pageInfo.endCursor });
-    const thirdPageData = thirdPage.edges.map((edge) => edge.node.toData());
-    expect(thirdPageData).toStrictEqual(dataList.slice(4, 6));
+      expect(user.toData()).toStrictEqual(data);
+    });
 
-    const backToSecondPage = await usersCollection.findAll({ last: PAGE_SIZE, before: thirdPage.pageInfo.startCursor });
-    const backToSecondPageData = backToSecondPage.edges.map((edge) => edge.node.toData());
-    expect(backToSecondPageData).toStrictEqual(dataList.slice(2, 4));
+    it("findManyByQuery", async () => {
+      const data1 = UserDoc.create({ displayName: "user-1" });
+      const data2 = UserDoc.create({ displayName: "user-2" });
 
-    const goToLastPage = await usersCollection.findAll({ last: PAGE_SIZE });
-    const goToLastPageData = goToLastPage.edges.map((edge) => edge.node.toData());
-    expect(goToLastPageData).toStrictEqual(dataList.slice(8, 10));
+      await usersRef.add(data1);
+      await usersRef.add(data2);
+
+      const [user1] = await usersCollection.findManyByQuery((ref) => ref.where("displayName", "==", "user-1"));
+
+      expect(user1.toData()).toStrictEqual(data1);
+    });
+
+    it("paginate", async () => {
+      const dataList = Array.from({ length: 10 }).map((_, idx) => {
+        let date = new Date("1999-12-31");
+        date = new Date(date.setDate(date.getDate() + idx));
+
+        const data = UserDoc.create({
+          displayName: `user-${idx}`,
+          createdAt: Timestamp.fromDate(date),
+        });
+        return data;
+      });
+
+      await Promise.all(dataList.map((data) => usersRef.add(data)));
+
+      const all = await usersCollection.findAll({});
+      expect(all.edges.map((edge) => edge.node.toData())).toStrictEqual(dataList);
+
+      const PAGE_SIZE = 2;
+
+      const firstPage = await usersCollection.findAll({ first: PAGE_SIZE });
+      const firstPageData = firstPage.edges.map((edge) => edge.node.toData());
+      expect(firstPageData).toStrictEqual(dataList.slice(0, 2));
+
+      const secondPage = await usersCollection.findAll({ first: PAGE_SIZE, after: firstPage.pageInfo.endCursor });
+      const secondPageData = secondPage.edges.map((edge) => edge.node.toData());
+      expect(secondPageData).toStrictEqual(dataList.slice(2, 4));
+
+      const thirdPage = await usersCollection.findAll({ first: PAGE_SIZE, after: secondPage.pageInfo.endCursor });
+      const thirdPageData = thirdPage.edges.map((edge) => edge.node.toData());
+      expect(thirdPageData).toStrictEqual(dataList.slice(4, 6));
+
+      const backToSecondPage = await usersCollection.findAll({
+        last: PAGE_SIZE,
+        before: thirdPage.pageInfo.startCursor,
+      });
+      const backToSecondPageData = backToSecondPage.edges.map((edge) => edge.node.toData());
+      expect(backToSecondPageData).toStrictEqual(dataList.slice(2, 4));
+
+      const goToLastPage = await usersCollection.findAll({ last: PAGE_SIZE });
+      const goToLastPageData = goToLastPage.edges.map((edge) => edge.node.toData());
+      expect(goToLastPageData).toStrictEqual(dataList.slice(8, 10));
+    });
+  });
+
+  describe("CollectionGroup", () => {
+    it("findOneById", async () => {
+      const userData = UserDoc.create({});
+      const { id } = await usersCollection.insert(userData);
+      const user = await usersCollection.findOneById(id);
+
+      const postData = PostDoc.create({ __id: "1" });
+      await user.postsCollection.insert({ id: "1", ...postData });
+
+      const post = await postsCollectionGroup.findOneById("1");
+
+      expect(post.toData()).toStrictEqual(postData);
+    });
+
+    it("findManyByQuery", async () => {
+      const users = await Promise.all(
+        Array.from({ length: 2 }).map(() =>
+          usersCollection.insert(UserDoc.create({})).then(({ id }) => usersCollection.findOneById(id))
+        )
+      );
+
+      const postData1 = PostDoc.create({ __id: "1", content: "post-1" });
+      await users.at(0).postsCollection.insert({ id: "1", ...postData1 });
+      const postData2 = PostDoc.create({ __id: "2", content: "post-2" });
+      await users.at(1).postsCollection.insert({ id: "2", ...postData2 });
+
+      const [post1] = await postsCollectionGroup.findManyByQuery((ref) => ref.where("content", "==", "post-1"));
+
+      expect(post1.toData()).toStrictEqual(postData1);
+    });
+
+    it("paginate", async () => {
+      const dataList = Array.from({ length: 10 }).map((_, idx) => {
+        let date = new Date("1999-12-31");
+        date = new Date(date.setDate(date.getDate() + idx));
+
+        const data = PostDoc.create({
+          content: `post-${idx}`,
+          createdAt: Timestamp.fromDate(date),
+        });
+        return data;
+      });
+
+      const users = await Promise.all(
+        Array.from({ length: 4 }).map(() =>
+          usersCollection.insert(UserDoc.create({})).then(({ id }) => usersCollection.findOneById(id))
+        )
+      );
+
+      await Promise.all(
+        dataList.map((data, idx) => users.at(idx % 4).postsCollection.insert({ id: data.__id, ...data }))
+      );
+
+      const all = await postsCollectionGroup.findAll({});
+      expect(all.edges.map((edge) => edge.node.toData())).toStrictEqual(dataList);
+
+      const PAGE_SIZE = 2;
+
+      const firstPage = await postsCollectionGroup.findAll({ first: PAGE_SIZE });
+      const firstPageData = firstPage.edges.map((edge) => edge.node.toData());
+      expect(firstPageData).toStrictEqual(dataList.slice(0, 2));
+
+      const secondPage = await postsCollectionGroup.findAll({ first: PAGE_SIZE, after: firstPage.pageInfo.endCursor });
+      const secondPageData = secondPage.edges.map((edge) => edge.node.toData());
+      expect(secondPageData).toStrictEqual(dataList.slice(2, 4));
+
+      const thirdPage = await postsCollectionGroup.findAll({ first: PAGE_SIZE, after: secondPage.pageInfo.endCursor });
+      const thirdPageData = thirdPage.edges.map((edge) => edge.node.toData());
+      expect(thirdPageData).toStrictEqual(dataList.slice(4, 6));
+
+      const backToSecondPage = await postsCollectionGroup.findAll({
+        last: PAGE_SIZE,
+        before: thirdPage.pageInfo.startCursor,
+      });
+      const backToSecondPageData = backToSecondPage.edges.map((edge) => edge.node.toData());
+      expect(backToSecondPageData).toStrictEqual(dataList.slice(2, 4));
+
+      const goToLastPage = await postsCollectionGroup.findAll({ last: PAGE_SIZE });
+      const goToLastPageData = goToLastPage.edges.map((edge) => edge.node.toData());
+      expect(goToLastPageData).toStrictEqual(dataList.slice(8, 10));
+    });
+  });
+
+  describe("Document", () => {
+    it("update", async () => {
+      const data = UserDoc.create({ displayName: "user-1" });
+      const { id } = await usersCollection.insert(data);
+      const user = await usersCollection.findOneById(id);
+
+      user.displayName = "user-99";
+      await user.update();
+
+      const userAfterUpdate = await usersCollection.findOneById(id);
+
+      expect(userAfterUpdate.toData()).toStrictEqual({ displayName: "user-99", createdAt: user.createdAt });
+    });
+
+    it("delete", async () => {
+      const data = UserDoc.create({});
+      const { id } = await usersCollection.insert(data);
+      const user = await usersCollection.findOneById(id);
+
+      await user.delete();
+
+      const exists = await usersRef
+        .doc(user.id)
+        .get()
+        .then((snap) => snap.exists);
+
+      expect(exists).toBe(false);
+    });
   });
 });
